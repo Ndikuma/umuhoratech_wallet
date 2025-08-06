049ef9e9911413ddfda69248f1f416570d7283b5
ndikuma <ii61212456@gmail.com>
2025-08-06 16:33:18 +0200

    add passage wsig

---
+++ passenger_wsgi.py
@@ -0,0 +1,11 @@
+import sys
+import os
+
+# Add your project directory to sys.path (adjust the path for your server)
+sys.path.insert(0, '/home/btcshulevgfhnehg/repositories/umuhoratech_wallet')
+
+# Set the Django settings module
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mini_wallet.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
