b786869531e0e5dd0f0aaa60fe0aa0dd2ece94b4
ndikuma <ii61212456@gmail.com>
2025-08-06 16:56:41 +0200

    add passage.json wsig

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -7,5 +7,7 @@ sys.path.insert(0, '/home/btcshulevgfhnehg/repositories/umuhoratech_wallet')
 # Set the Django settings module
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mini_wallet.settings")
 
+
+
 from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
