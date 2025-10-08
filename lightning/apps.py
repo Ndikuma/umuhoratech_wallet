f03870abbd5c66baf14796f1ccddfa2ca803a065
ndikuma <ii61212456@gmail.com>
2025-10-08 20:36:49 +0200

    add signal

--- lightning/apps.py
+++ lightning/apps.py
@@ -6,7 +6,7 @@ class LightningConfig(AppConfig):
     name = "lightning"
     
     def ready(self):
-        # import lightning.signals
+        import lightning.signals
         # from .blink_ws_scheduler import schedule_blink_ws
         # schedule_blink_ws()
         pass
